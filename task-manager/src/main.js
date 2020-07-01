cfe7e7f8acb455916932060c152378af00802ebb
Lobo, Eliel <elobo@cvent.com>
2020-06-30 22:30:59 -0500

    added endpoints to get all and getById for users and tasks

--- task-manager/src/main.js
+++ task-manager/src/main.js
@@ -8,26 +8,69 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+app.get('/users', (req, res) => {
+
+    User.find().then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send(e.message)
+    }) 
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id
+    User.findOne({_id: id}).then((user) => {
+        if (user) {
+            res.send(user)
+        } else {
+            res.status(400).send({message: "User not found"})
+        }
+    }).catch((e) => {
+        res.status(500).send({ message: e.message })
+    }) 
+})
+
 app.post('/users', (req, res) => {
     const user = new User(req.body)
 
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
     }).catch((e) => {
         res.status(400).send(e)
-    })
-    
+    }) 
+})
+
+
+app.get('/tasks', (req, res) => {
+
+    Task.find().then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send({ message: e.message })
+    }) 
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const id = req.params.id
+    Task.findOne({_id: id}).then((task) => {
+        if (task) {
+            res.send(task)
+        } else {
+            res.status(400).send({message: "Task not found"})
+        }
+    }).catch((e) => {
+        res.status(500).send({ message: e.message })
+    }) 
 })
 
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
     task.save().then(() => {
-        res.send(task)
+        res.status(201).send(task)
     }).catch((e) => {
         res.status(400).send(e)
-    })
-    
+    })  
 })
 
 

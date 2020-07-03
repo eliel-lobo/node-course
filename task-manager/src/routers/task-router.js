19313342401c313a5af0022e005655c3963a7c92
Lobo, Eliel <elobo@cvent.com>
2020-07-02 20:52:46 -0500

    added filtering and pagination to GET /tasks endpoint

--- task-manager/src/routers/task-router.js
+++ task-manager/src/routers/task-router.js
@@ -5,10 +5,29 @@ const Task = require('../db/models/task')
 
 
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+    const options = {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.offset),
+        sort: {}
+    }
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split('_')
+        options.sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
 
     try { 
-        const tasks = await Task.find({ owner: req.user._id})
-        res.send(tasks)
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options
+        }).execPopulate()
+        res.send(req.user.tasks)
     } catch(e) {
         res.status(500).send({ message: e.message })
     } 
